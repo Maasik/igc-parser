8dbf6a6fad256aaf2e66ae87051038f40ce26c26
Theodore Messinezis <theo@theomessin.com>
2017-07-28 22:37:57 +0100

    Fix coordinate conversion

--- src/Repository/Helpers.php
+++ src/Repository/Helpers.php
@@ -6,16 +6,16 @@ class Helpers
 {
     public static function coordinates($d)
     {
-        return [
-            'latitude' =>  self::convertCoordinates('0' . substr($d, 0, 8)),
-            'longitude' => self::convertCoordinates(substr($d, 8, 9))
-        ];
-    }
+        $latitude = floatval(substr($d, 0, 2)) +
+        floatval(substr($d, 2, 5)) / 60000.0;
+        if ($d[7] === 'S') $latitude *= -1;
 
-    private static function convertCoordinates($d)
-    {
-        $sign = $d[8] === 'N' || $d[8] ==='E' ? 1 : -1;
-        return $sign * ((int) substr($d, 0, 3) + (int) substr($d, 3, 2) / 60 + (int) substr($d, 5, 3) / 3600);
+
+        $longitude = floatval(substr($d, 8, 3)) +
+            floatval(substr($d, 11, 5)) / 60000.0;
+        if ($d[16] === 'W') $longitude *= -1;
+
+        return [ 'latitude' =>  $latitude, 'longitude' => $longitude ];
     }
 
     public static function feet($metres)
